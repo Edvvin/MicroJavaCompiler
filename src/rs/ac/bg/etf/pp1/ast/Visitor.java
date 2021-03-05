7c78dd074d99bad1d05c8853e28684773ee06950
Edvin Maid <edvvin@mail.com>
2021-03-05 23:23:35 +0100

    Ternary fix

---
+++ src/rs/ac/bg/etf/pp1/ast/Visitor.java
@@ -0,0 +1,98 @@
+// generated with ast extension for cup
+// version 0.8
+// 5/2/2021 23:11:30
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public interface Visitor { 
+
+    public void visit(Factor Factor);
+    public void visit(DesignatorStatement DesignatorStatement);
+    public void visit(Expr1 Expr1);
+    public void visit(ActualParamList ActualParamList);
+    public void visit(CondFact CondFact);
+    public void visit(FormalParamList FormalParamList);
+    public void visit(FormPars FormPars);
+    public void visit(VarDeclList VarDeclList);
+    public void visit(SumExpr SumExpr);
+    public void visit(MethodDeclList MethodDeclList);
+    public void visit(Statement Statement);
+    public void visit(Relop Relop);
+    public void visit(CondTerm CondTerm);
+    public void visit(Condition Condition);
+    public void visit(StatementList StatementList);
+    public void visit(ActualPars ActualPars);
+    public void visit(Letop Letop);
+    public void visit(Ltop Ltop);
+    public void visit(Getop Getop);
+    public void visit(Gtop Gtop);
+    public void visit(Eqop Eqop);
+    public void visit(Modop Modop);
+    public void visit(Divop Divop);
+    public void visit(Mulop Mulop);
+    public void visit(Subop Subop);
+    public void visit(Addop Addop);
+    public void visit(IndexDesig IndexDesig);
+    public void visit(Designator Designator);
+    public void visit(CondRelop CondRelop);
+    public void visit(CondExpr CondExpr);
+    public void visit(CondFacts CondFacts);
+    public void visit(CondFact CondFact);
+    public void visit(CondTerms CondTerms);
+    public void visit(CondTerm CondTerm);
+    public void visit(ActualParam ActualParam);
+    public void visit(ActualParams ActualParams);
+    public void visit(NoActuals NoActuals);
+    public void visit(Actuals Actuals);
+    public void visit(NewObj NewObj);
+    public void visit(FuncCall FuncCall);
+    public void visit(Var Var);
+    public void visit(BraceExpr BraceExpr);
+    public void visit(BoolConst BoolConst);
+    public void visit(CharConst CharConst);
+    public void visit(NumConst NumConst);
+    public void visit(Terms Terms);
+    public void visit(Term Term);
+    public void visit(TermExpr TermExpr);
+    public void visit(AddExpr AddExpr);
+    public void visit(PosExpr PosExpr);
+    public void visit(NegExpr NegExpr);
+    public void visit(Expr Expr);
+    public void visit(TerExpr TerExpr);
+    public void visit(StmtList StmtList);
+    public void visit(DoWhile DoWhile);
+    public void visit(MatchedIf MatchedIf);
+    public void visit(UnmatchedIf UnmatchedIf);
+    public void visit(ReturnNoExpr ReturnNoExpr);
+    public void visit(ReturnExpr ReturnExpr);
+    public void visit(ContinueStmt ContinueStmt);
+    public void visit(BreakStmt BreakStmt);
+    public void visit(ReadStmt ReadStmt);
+    public void visit(PrintStmtParam PrintStmtParam);
+    public void visit(PrintStmt PrintStmt);
+    public void visit(ErrAssignment ErrAssignment);
+    public void visit(DesignatorStatement DesignatorStatement);
+    public void visit(FuncCallStmt FuncCallStmt);
+    public void visit(DecStmt DecStmt);
+    public void visit(IncStmt IncStmt);
+    public void visit(EqualStmt EqualStmt);
+    public void visit(NoStmt NoStmt);
+    public void visit(Statements Statements);
+    public void visit(FormalParamDecl FormalParamDecl);
+    public void visit(SingleFormalParamDecl SingleFormalParamDecl);
+    public void visit(FormalParamDecls FormalParamDecls);
+    public void visit(NoFormParam NoFormParam);
+    public void visit(FormParams FormParams);
+    public void visit(MethodTypeName MethodTypeName);
+    public void visit(MethodDecl MethodDecl);
+    public void visit(NoMethodDecl NoMethodDecl);
+    public void visit(MethodDeclarations MethodDeclarations);
+    public void visit(Type Type);
+    public void visit(VarDecl VarDecl);
+    public void visit(NoVarDecl NoVarDecl);
+    public void visit(VarDeclarations VarDeclarations);
+    public void visit(ProgName ProgName);
+    public void visit(Program Program);
+
+}
